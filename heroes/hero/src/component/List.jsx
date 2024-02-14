53ba4c379b0385481ecd3ab297b6d1a7273e12de
Amira <tulebaevaamira62@gmail.com>
2024-02-14 12:43:49 +0600

    h2 add

--- heroes/hero/src/component/List.jsx
+++ heroes/hero/src/component/List.jsx
@@ -7,6 +7,7 @@ class List extends React.Component {
             <div className="List">
                 <div className="name">
                     <img src={url} alt={name}/>
+                    <h2 className="name">{name}</h2>
                     <h4 className="occupation">{occupation}</h4>
                     <p className="info">{info}</p>
                 </div>
