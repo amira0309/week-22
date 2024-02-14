64df2a236d55d90c9f9a3bf5ae563139d61d1b3f
Amira <tulebaevaamira62@gmail.com>
2024-02-14 10:42:11 +0600

    изменила класс

--- heroes/hero/src/component/List.jsx
+++ heroes/hero/src/component/List.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
+
 class List extends React.Component {
     render() {
-				const {name, occupation, url, info} = this.props;
+        const { name, occupation, url, info } = this.props;
         return (
-            <div className="ListHeroes">
+            <div className="List">
                 <div className="name">
                     <img src={url} alt={name}/>
                     <h4 className="occupation">{occupation}</h4>
                     <p className="info">{info}</p>
                 </div>
-			</div>
-);
+            </div>
+        );
+    }
 }
-}
+
 export default List;
