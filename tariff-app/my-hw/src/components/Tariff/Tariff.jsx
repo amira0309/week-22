1568115c489c6e22eeb876285ac32cfabbcab40e
Amira <tulebaevaamira62@gmail.com>
2024-02-28 14:37:40 +0600

    классовый в функцилнальный поменяла

--- tariff-app/my-hw/src/components/Tariff/Tariff.jsx
+++ tariff-app/my-hw/src/components/Tariff/Tariff.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import './Tariff.css';
 
-class Tariff extends React.Component {
-  handleClick = () => {
-    if (this.props.onClick) {
-      this.props.onClick();
+const Tariff = ({ name, price, highlighted, type, speed, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
     }
   };
 
-  render() {
-    const { name, price, highlighted, type, speed } = this.props;
-    return (
-      <div className={`tariff ${highlighted ? 'highlighted' : ''} ${type}`} onClick={this.handleClick}>
-        <h3>{name}</h3>
-        <p>руб {price}/мес</p>
-        <p>Скорость: {speed}</p>
-        <p>Объем включенного трафика не ограничен</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={`tariff ${highlighted ? 'highlighted' : ''} ${type}`} onClick={handleClick}>
+      <h3>{name}</h3>
+      <p>руб {price}/мес</p>
+      <p>Скорость: {speed}</p>
+      <p>Объем включенного трафика не ограничен</p>
+    </div>
+  );
+};
 
 export default Tariff;
-
