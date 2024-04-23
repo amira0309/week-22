49b69f076f8655594b5a42b6043a94f49b57091a
Amira <tulebaevaamira62@gmail.com>
2024-04-23 09:43:07 +0600

    добавила изменения

--- word-app/word-app/src/components/EditableTable/EditableTable.jsx
+++ word-app/word-app/src/components/EditableTable/EditableTable.jsx
@@ -1,75 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const EditableTable = ({ data }) => {
-  const [editMode, setEditMode] = useState(false);
-  const [editedData, setEditedData] = useState(data);
-
-  const handleEdit = () => {
-    setEditMode(true);
-  };
-
-  const handleChange = (e, index) => {
-    const { name, value } = e.target;
-    const newData = [...editedData];
-    newData[index][name] = value;
-    setEditedData(newData);
-  };
-
-  const handleCancelEdit = () => {
-    setEditMode(false);
-    setEditedData(data);
-  };
-
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Слово</th>
-            <th>Перевод</th>
+    <table>
+      <thead>
+        <tr>
+          <th>Слово</th>
+          <th>Перевод</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.word}</td>
+            <td>{item.translation}</td>
           </tr>
-        </thead>
-        <tbody>
-          {editedData.map((item, index) => (
-            <tr key={index}>
-              <td>
-                {editMode ? (
-                  <input
-                    type="text"
-                    name="word"
-                    value={item.word}
-                    onChange={(e) => handleChange(e, index)}
-                  />
-                ) : (
-                  item.word
-                )}
-              </td>
-              <td>
-                {editMode ? (
-                  <input
-                    type="text"
-                    name="translation"
-                    value={item.translation}
-                    onChange={(e) => handleChange(e, index)}
-                  />
-                ) : (
-                  item.translation
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {editMode ? (
-        <div>
-          <button onClick={handleCancelEdit}>Отмена редактирования</button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={handleEdit}>Редактировать</button>
-        </div>
-      )}
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
