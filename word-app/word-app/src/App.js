a886853b4f42ece964e8875dc18131dc886dd8dc
Amira <tulebaevaamira62@gmail.com>
2024-05-04 14:56:45 +0600

    добавила маршруты

--- word-app/word-app/src/App.js
+++ word-app/word-app/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import EditableTable from './components/EditableTable/EditableTable';
 import CardGame from './components/CardGame/CardGame';
-import NavBar from './components/NavBar';
-import './styles.css'; 
+import WordComponents from './components/WordComponents/WordComponents';
+import './styles.css';
 
 const App = () => {
   const data = [
@@ -15,19 +15,25 @@ const App = () => {
 
   return (
     <Router>
-        <div className="content-container">
-          <Menu />
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<div>
-                <h2>Слова</h2>
-                <EditableTable data={data} />
-              </div>}
-            />
-            <Route path="/game" element={<CardGame />} />
-            
-            </Routes>
-        </div>
+      <div className="content-container">
+        {/* Отображение меню на всех страницах */}
+        <Menu />
+
+        {/* Определение маршрутов */}
+        <Routes>
+          {/* Маршрут для главной страницы (компонент EditableTable) */}
+          <Route path="/" element={<div>
+            <h2>Слова</h2>
+            <EditableTable data={data} />
+          </div>} />
+
+          {/* Маршрут для страницы с карточками (компонент CardGame) */}
+          <Route path="/game" element={<CardGame />} />
+        </Routes>
+
+        {/* Дополнительные компоненты, не связанные с маршрутизацией */}
+        <WordComponents />
+      </div>
     </Router>
   );
 };
--- word-app/word-app/src/components/Menu.jsx
+++ word-app/word-app/src/components/Menu.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Menu.css'; 
+import './Menu.css';
+import Logo from '../images/logo1.jpg'; // Импортируйте изображение логотипа
 
 const Menu = () => {
   return (
     <div className="menu">
       <div className="logo">
-        <Link to="/">Логотип</Link>
+        {/* Используйте Link для логотипа */}
+        <Link to="/" className="logo-link">
+          Логотип
+        </Link>
       </div>
       <div className="menu-links">
         <Link to="/">Главная</Link>
-        <Link to="/game">Игра с карточками</Link>
+        <Link to="/game">Тренажер карточек</Link>
       </div>
     </div>
   );
 };
 
 export default Menu;
-
