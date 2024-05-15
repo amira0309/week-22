f1968359f9be95065c059e213482c3d5764bcc9f
Amira <tulebaevaamira62@gmail.com>
2024-05-15 15:37:45 +0600

    useEffect добавила

--- word-app/word-app/src/components/TextFormatter.jsx
+++ word-app/word-app/src/components/TextFormatter.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const TextFormatter = () => {
   const [inputText, setInputText] = useState('');
   const [formattedText, setFormattedText] = useState('');
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    // Установка фокуса на поле ввода при загрузке компонента
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []); // Пустой массив зависимостей означает, что эффект будет вызываться только при монтировании компонента
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
   const handleFormatText = () => {
-    const formatted = inputText.toUpperCase(); 
+    const formatted = inputText.toUpperCase();
     setFormattedText(formatted);
     setInputText('');
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      // Обработка нажатия клавиши Enter для показа перевода
+      handleFormatText(); // Вызываем функцию форматирования текста при нажатии Enter
+    }
   };
 
   return (
@@ -20,9 +35,11 @@ const TextFormatter = () => {
         type="text"
         value={inputText}
         onChange={handleInputChange}
+        onKeyPress={handleKeyPress} // Добавляем обработчик нажатия клавиши Enter
+        ref={inputRef} // Привязываем ref к элементу ввода
         placeholder="Введите текст"
       />
-      <button onClick={handleFormatText}>Добавить</button>
+      <button onClick={handleFormatText}>Форматировать</button>
       {formattedText && (
         <p style={{ textTransform: 'uppercase' }}>{formattedText}</p>
       )}
